c7dc400c1ed811f62f12fe5409321bf91d8b9525
Lyes-Azzouz <azzouz.lyes.w@gmail.com>
2024-05-15 00:23:08 +0200

    ajout attribut title dans les boutons des cards

--- front-end/src/components/Pages_components/MesCodes_components/ButtonsCard/ButtonsCard.jsx
+++ front-end/src/components/Pages_components/MesCodes_components/ButtonsCard/ButtonsCard.jsx
@@ -8,9 +8,15 @@ import { MdOpenInNew } from "react-icons/md";
 export function ButtonsCard() {
   return (
     <div className="btn-card__container">
-      <button id="btn-1">{<RiDeleteBin6Fill />} </button>
-      <button id="btn-2">{<MdOpenInNew />} </button>
-      <button id="btn-3">{<BiSolidMessageSquareEdit />} </button>
+      <button id="btn-1" title="Supprimer">
+        {<RiDeleteBin6Fill />}
+      </button>
+      <button id="btn-2" title="Ouvrir">
+        {<MdOpenInNew />}
+      </button>
+      <button id="btn-3" title="Modifier">
+        {<BiSolidMessageSquareEdit />}
+      </button>
     </div>
   );
 }
