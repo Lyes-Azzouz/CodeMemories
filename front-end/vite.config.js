6e6cea84a5ffae3d4e48129503124c3295f4ea25
Lyes-Azzouz <azzouz.lyes.w@gmail.com>
2024-07-01 12:06:03 +0200

    maj

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      external: ["react-router-dom", "react-dom", "firebase/app"], // Ajoutez firebase/app ici
+    },
+  },
   optimizeDeps: {
     include: ["react-router-dom"],
-
+    exclude: ["firebase/app"],
     css: {
       modules: {},
       postcss: {},
